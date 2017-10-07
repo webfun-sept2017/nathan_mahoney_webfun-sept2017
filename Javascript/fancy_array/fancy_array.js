b464c21da14e2d20f98fb17b5caa779dd8b6a7bc
Nathan Mahoney <nortmahoney@gmail.com>
2017-10-07 01:26:53 -0400

    removing CSS and JavaScript directories

--- Javascript/fancy_array/fancy_array.js
+++
@@ -1,9 +0,0 @@
-var names = [ "James", "Jill", "Jane", "Jack" ];
-function fancyArray() 
-{
-	for (var i = 0; i < names.length; i++) 
-	{
-		console.log(i + " -> " + names[i]);	
-	}
-}
-fancyArray(names);
--- Javascript/for_a_few_billion/for_a_few_billion.js
+++
@@ -1,6 +0,0 @@
-var start = 0.01;
-for (var i = 0; i < 31; i++) {
-	console.log(start);
-	start += start;
-}
-
